aeb8392883613c457f93fa9dbb944b849639d08a
gregjurman <gdj2214@rit.edu>
2011-04-22 02:12:26 -0400

    Editing the test file quick for some timing stats.

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 import Image
 import tesserwrap
+import time
+import subprocess
 
 im = Image.open("test.png")
 tw = tesserwrap.Tesserwrap()
 
-# We want a single line
-tw.setpagesegmode(tesserwrap.pagesegmode.auto)
 
-# Do it up
-print tw.tesseractrect(im.tostring(), 1, im.size[0], 0,0, im.size[0], im.size[1])
+time_start = time.time()
 
+# Do it up 1000 times
+for i in range(1000):
+    tw.tesseractrect(im.tostring(), 1, im.size[0], 0,0, im.size[0], im.size[1])
+time_end = time.time()
+print "tesserwrap:"
+print "    Total Time: %f" % (time_end - time_start)
+print "    Time per iteration: %f" % ((time_end - time_start)/1000)
 
