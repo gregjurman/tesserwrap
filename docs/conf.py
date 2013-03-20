b541015d815550d08a7e52cbbf9e5485da52c53e
Greg Jurman <gdj2214@rit.edu>
2013-03-20 14:00:09 -0700

    Fix for RTFD not importing module

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-
+import os
 import sys
+
+sys.path.append(os.path.abspath(".."))
 
 class Mock(object):
     def __init__(self, *args, **kwargs):
@@ -35,6 +36,8 @@ class Mock(object):
             return Mock()
 
 sys.modules['tesserwrap.core'] = Mock()
+
+import tesserwrap
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
